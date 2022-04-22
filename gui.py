c61d111e442d70dd07cd7cc864f1f9e7323bf3c5
amirsed76 <amirreza.seddighin1376@gmail.com>
2022-04-22 07:17:00 -0700

    plotting

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from tkinter import *
+from tkinter import messagebox
+
 from plotting import Plot
 import random
 import threading
@@ -18,38 +20,55 @@ class GUI:
         self.allow_plotting = False
         self.window = Tk()
         self.window.title('Plotting in Tkinter')
-        self.window.geometry("500x500")
         x, y = 40, 40
-        label_width = 100
-        label_height = 40
+        label_width, label_height = 100, 40
         padding = 10
+        input_width, input_height = 30, 100
 
         time1_label = Label(self.window, text="arduino start time: ").place(x=x, y=y)
-        time1_label_input = Entry(self.window, width=30).place(x=x + label_width + padding, y=y)
+        self.time1_input = Entry(self.window, width=input_width)
+        self.time1_input.place(x=x + label_width + padding, y=y)
 
         new_y = y + label_height + padding
         time2_label = Label(self.window, text="Modules off time: ").place(x=x, y=new_y)
-        time2_label = Entry(self.window, width=30).place(x=x + label_width + padding, y=new_y)
+        self.time2_input = Entry(self.window, width=input_width)
+        self.time2_input.place(x=x + label_width + padding, y=new_y)
 
         new_y = y + 2 * (label_height + padding)
         time3_label = Label(self.window, text="arduino off time: ").place(x=x, y=new_y)
-        time3_label = Entry(self.window, width=30).place(x=x + label_width + padding, y=new_y)
+        self.time3_input = Entry(self.window, width=input_width)
+        self.time3_input.place(x=x + label_width + padding, y=new_y)
 
         new_y = y + 3 * (label_height + padding)
-        submit_button = Button(self.window, text="plot", command=self.plotting, ).place(x=x, y=new_y)
+        button = Button(self.window, text="plot", command=self.plotting, ).place(x=x, y=new_y)
+        self.window.geometry(f"{x + label_width + padding + 200}x{new_y + 100}")
+        self.window.resizable(False, False)
 
         self.window.mainloop()
 
     def plotting(self):
+        # try:
+        #     start_time = int(self.time1_input.get())
+        #     pick_time = int(self.time2_input.get())
+        #     end_time = int(self.time3_input.get())
+        # except:
+        #     messagebox.showerror('خطا', 'اعداد وارد شده معتبر نمیباشند')
+        #     return
+
         self.allow_plotting = True
         self.window.withdraw()
         self.plot = Plot()
         self.plot.set_after_close(func=self.after_close_plot)
         for i in range(10):
-            number = random.randint(0, 10)
+            number1 = random.randint(0, 10)
+            number2 = random.randint(0, 10)
+            number3 = random.randint(0, 10)
+            self.plot.update_line(line_name="a", y=number1)
+            self.plot.update_line(line_name="b", y=number2)
+            self.plot.update_line(line_name="c", y=number3)
+            self.plot.update_plot()
             if not self.allow_plotting:
                 return
-            self.plot.update_plot(number)
             time.sleep(0.2)
         now = datetime.now()
         self.plot.save(
--- plotting.py
+++ plotting.py
@@ -1,25 +1,38 @@
 import matplotlib.pyplot as plt
 import time
+import matplotlib.pylab as pl
+import numpy as np
+
+from matplotlib import cycler
+
 
 class Plot:
+    colors = ['blue', 'orange', 'purple', 'green', 'red', "brown", "pink", "gray", "olive", "cyan"]
+
     def __init__(self):
-        self._init_line()
-
-    def _init_line(self):
         plt.ion()
-        self._figure = plt.figure(figsize=(7,7))
+        self._figure = plt.figure(figsize=(7, 7))
         self._ax = self._figure.add_subplot(111)
-        self._line, = self._ax.plot([], [], 'b-')
+        self._lines = dict()
 
-    def _update_line(self, y, x=None):
-        if x is None:
-            x = len(self._line.get_xdata())
+    def update_line(self, line_name, y, x=None):
+        if line_name not in self._lines.keys():
+            x = 0 if x is None else x
+            color = self.colors[len(self._lines.keys()) % len(self.colors)]
+            self._lines[line_name], = self._ax.plot([x], [y], 'b-', label=line_name, color=color)
+            plt.legend(loc="upper right")
 
-        xs, ys = list(self._line.get_xdata()) + [x], list(self._line.get_ydata()) + [y]
-        self._line, = self._ax.plot(xs, ys, 'b-')
 
-    def update_plot(self, y, x=None):
-        self._update_line(y=y, x=x)
+
+        else:
+            line = self._lines[line_name]
+            x = len(line.get_xdata()) if x is None else x
+
+            xs, ys = list(line.get_xdata()) + [x], list(line.get_ydata()) + [y]
+            color = self.colors[list(self._lines.keys()).index(line_name) % len(self.colors)]
+            self._lines[line_name], = self._ax.plot(xs, ys, 'b-', label=line_name, color=color)
+
+    def update_plot(self):
         self._figure.canvas.draw()
         self._figure.canvas.flush_events()
 
